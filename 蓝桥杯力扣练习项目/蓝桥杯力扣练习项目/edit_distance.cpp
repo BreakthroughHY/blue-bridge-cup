b338f62ca59dff9ebbf0817ab69ebcb8bae413f7
86184 <86184@>
2023-03-09 17:30:55 +0800

    2号机临时提交

--- 蓝桥杯力扣练习项目/蓝桥杯力扣练习项目/edit_distance.cpp
+++ 蓝桥杯力扣练习项目/蓝桥杯力扣练习项目/edit_distance.cpp
@@ -7,7 +7,7 @@ int editDistance()
 
 	DWORD start_time = GetTickCount();
 
-	cout << s.minDistance("", "") << endl;
+	cout << s.minDistance("asdfssqwes", "ass") << endl;
 
 	DWORD end_time = GetTickCount();
 
@@ -18,6 +18,74 @@ int editDistance()
 
 int SolutionEditDistance::minDistance(string word1, string word2)
 {
+	int n = word1.size(); // 原字符串长度
+	int m = word2.size(); // 目标字符串长度
+	vector<vector<int>> dp(n, vector<int>(m, 1));
 
-	return 0;
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			if (word1[i] == word2[j])
+				dp[i][j] = 0; // 出现相同字符将对应dp数组值设为0
+			cout << dp[i][j] << "	";
+		}
+		cout << endl;
+		cout << endl;
+	}
+	cout << "-------------------------------------" << endl;
+	// 寻找dp数组内的最优路径
+	// 同样通过动态规划寻找最优路径   空间复杂度n，时间复杂度n*m
+
+	vector<int> dp_(m);
+
+	// 初始化dp_数组
+	for (int i = 0; i < m; ++i)
+	{
+		if (!i) // i等于0特殊处理
+			dp_[i] = dp[0][i];
+		else
+			dp_[i] = dp_[i - 1] + 1;
+		cout << dp_[i] << "	";
+	}
+	cout << endl;
+	int temp, temp1;
+	for (int i = 1; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			if (!j) // j 等于0特殊处理
+			{
+				temp1 = dp_[j];
+				dp_[j] = dp_[j] + 1;
+				cout << dp_[j] << "	";
+			}
+			else
+			{
+				temp = temp1;
+				temp1 = dp_[j];
+				if (temp <= min(dp_[j - 1], dp_[j]))
+				{
+					dp_[j] = dp[i][j] + temp;
+					cout << dp_[j] << "	";
+				}
+				else
+				{
+					if (dp_[j - 1] > dp_[j])
+					{
+						dp_[j] = dp_[j] + 1;
+						cout << dp_[j] << "	";
+					}
+					else
+					{
+						dp_[j] = dp_[j - 1] + 1;
+						cout << dp_[j] << "	";
+					}
+				}
+			}
+		}
+		cout << endl;
+	}
+
+	return dp_[m - 1];
 }
