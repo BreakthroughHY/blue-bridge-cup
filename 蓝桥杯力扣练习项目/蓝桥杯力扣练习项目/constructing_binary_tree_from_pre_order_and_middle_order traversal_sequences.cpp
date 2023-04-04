c16391b04d9dabe421a3c170035c0ca02df73f4f
BreakthroughHY <114470527+BreakthroughHY@users.noreply.github.com>
2023-04-04 08:31:10 +0800

    1号机临时上传

---
+++ 蓝桥杯力扣练习项目/蓝桥杯力扣练习项目/constructing_binary_tree_from_pre_order_and_middle_order traversal_sequences.cpp
@@ -0,0 +1,62 @@
+#include "constructing_binary_tree_from_pre_order_and_middle_order traversal_sequences.h"
+
+int onceUponTimeSequenceTheSequenceStructureBinaryTreeTraversalSequence()
+{
+	// 创建对象
+	SolutionConstructingBinaryTreeFromPreOrderandMiddleOrderTraversalSequences s;
+	vector<int> v = { 3,9,20,15,7 };
+	vector<int> vv = { 9,3,15,20,7 };
+
+	DWORD start_time = GetTickCount();
+
+	TreeNode* tree = s.buildTree(v, vv);
+
+	DWORD end_time = GetTickCount();
+
+	cout << "The run time is:" << (end_time - start_time) << "ms!" << endl;//输出运行时间
+
+	binaryTreeSequenceTraversal(tree);
+
+	return 0;
+}
+
+TreeNode* SolutionConstructingBinaryTreeFromPreOrderandMiddleOrderTraversalSequences::buildTree(vector<int>& preorder, vector<int>& inorder)
+{
+	int n = inorder.size();
+
+	for (int i = 0; i < n; ++i)
+		index[inorder[i]] = i;
+
+	return myBuildTree()
+}
+
+TreeNode* SolutionConstructingBinaryTreeFromPreOrderandMiddleOrderTraversalSequences::myBuildTree(vector<int>& preorder, vector<int>& inorder)
+{
+
+
+
+
+}
+
+// 二叉树的层序遍历
+void binaryTreeSequenceTraversal(TreeNode* root)
+{
+	queue<TreeNode*> q;
+	
+	if (root)
+		return;
+
+	q.push(root);
+	TreeNode* temp = nullptr;
+	while (!q.empty())
+	{
+		temp = q.front();
+		q.pop();
+		cout << temp->val << endl;
+		if (temp->left)
+			q.push(temp->left);
+
+		if (temp->right)
+			q.push(temp->right);
+	}
+}
---
+++ 蓝桥杯力扣练习项目/蓝桥杯力扣练习项目/constructing_binary_tree_from_pre_order_and_middle_order traversal_sequences.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#ifndef __CONSTRUCTING_BINARY_TREE_FROM_PRE_ORDER_AND_MIDDLE_ORDER_TRAVERSAL_SEQUENCES_H__
+#define __CONSTRUCTING_BINARY_TREE_FROM_PRE_ORDER_AND_MIDDLE_ORDER_TRAVERSAL_SEQUENCES_H__
+
+#include <queue> // 写一个二叉树的层序遍历
+#include <unordered_map>
+#include "main.h"
+
+// 对应结构体声明
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+// 类声明
+class SolutionConstructingBinaryTreeFromPreOrderandMiddleOrderTraversalSequences {
+private:
+	unordered_map<int, int> index;
+public:
+	TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder);
+
+	TreeNode* myBuildTree(vector<int>& preorder, vector<int>& inorder);
+};
+
+int onceUponTimeSequenceTheSequenceStructureBinaryTreeTraversalSequence();
+
+// 二叉树的层序遍历
+void binaryTreeSequenceTraversal(TreeNode* root);
+
+#endif // !__CONSTRUCTING_BINARY_TREE_FROM_PRE_ORDER_AND_MIDDLE_ORDER_TRAVERSAL_SEQUENCES_H__
