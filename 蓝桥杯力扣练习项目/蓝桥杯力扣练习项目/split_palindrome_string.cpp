a7533bb638773137bd221ab4a9ba8df710fcfab5
BreakthroughHY <114470527+BreakthroughHY@users.noreply.github.com>
2023-04-10 09:37:27 +0800

    分割回文串

--- 蓝桥杯力扣练习项目/蓝桥杯力扣练习项目/split_palindrome_string.cpp
+++ 蓝桥杯力扣练习项目/蓝桥杯力扣练习项目/split_palindrome_string.cpp
@@ -7,33 +7,73 @@ int splitPalindromeString()
 
 	DWORD start_time = GetTickCount();
 
-	vector<vector<string>> v = s.partition("aab");
+	vector<vector<string>> v = s.partition("abbab");
 
 	DWORD end_time = GetTickCount();
 
 	cout << "The run time is:" << (end_time - start_time) << "ms!" << endl;//输出运行时间
 
+	s.printV(v);
+
 	return 0;
 }
 
+// 自写方法   和官方第二种方法思路相同
 vector<vector<string>> SolutionSplitPalindromeString::partition(string s)
 {
 	int n = s.size();
-	vector<vector<string>> ret;
 	vector<string> temp;
 	int left, right;
 
 	for (int i = 0; i < n; ++i)
 	{
-		temp.clear();
 		left = right = i;
-		temp.emplace_back(s.substr());
-		while ()
+		dict[left].emplace_back(right);
+		while (--left >= 0 && ++right < n && s[left] == s[right])
 		{
-
+			dict[left].emplace_back(right);
 		}
-		ret.emplace_back(temp.begin(), temp.end());
+		if (i == n - 1)
+			break;
+
+		left = i;
+		right = i + 1;
+
+		while (left >= 0 && right < n && s[left] == s[right])
+		{
+			dict[left].emplace_back(right);
+			++right;
+			--left;
+		}
 	}
+
+	dfs(temp, 0, s);
 
 	return ret;
 }
+
+void SolutionSplitPalindromeString::dfs(vector<string>& temp, int left, string& s)
+{
+	if (dict.find(left) == dict.end())
+	{
+		ret.emplace_back(temp.begin(), temp.end());
+		return;
+	}
+	for (auto right : dict[left])
+	{
+		temp.emplace_back(s.substr(left, right - left + 1));
+		dfs(temp, right + 1, s);
+		temp.pop_back();
+	}
+}
+
+void SolutionSplitPalindromeString::printV(vector<vector<string>>& v)
+{
+	for (auto& fa : v)
+	{
+		for (auto& so : fa)
+			cout << so << "	";
+		cout << endl;
+	}
+}
+
--- 蓝桥杯力扣练习项目/蓝桥杯力扣练习项目/split_palindrome_string.h
+++ 蓝桥杯力扣练习项目/蓝桥杯力扣练习项目/split_palindrome_string.h
@@ -3,12 +3,20 @@
 #ifndef __SPLIT_PALINDROME_STRING_H__
 #define __SPLIT_PALINDROME_STRING_H__
 
+#include <unordered_map>
 #include "main.h"
 
 // ÀàÉùÃ÷
 class SolutionSplitPalindromeString {
+private:
+    vector<vector<string>> ret;
+    unordered_map<int, vector<int>> dict;
 public:
     vector<vector<string>> partition(string s);
+
+    void printV(vector<vector<string>>& v);
+
+    void dfs(vector<string>& temp, int left, string& s);
 };
 
 int splitPalindromeString();
